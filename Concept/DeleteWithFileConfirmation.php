db277fa2dca79acfba187e400f7f1adc7f534d6d
Ashish <42094101+Candy128x@users.noreply.github.com>
2018-10-18 20:39:00 +0530

    DeleteWithFileConfirmation

---
+++ Concept/DeleteWithFileConfirmation.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>DeleteWithFileConfirmation</title>
+</head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
+<body>
+
+<form action="#" method="post">
+<input type="file" name="uploadFile" id="uploadFileId" size="30" /> 
+<input type="submit" name="submit" id="uploadSubmitId" value="Submit" />
+</form>
+
+<script type="text/javascript">
+$(document).ready(function() {
+	$('#uploadSubmitId').bind("click",function() 
+	{ 
+		var imgVal = $('#uploadFileId').val(); 
+		if(imgVal=='') 
+	{ 
+		alert("Please Select File.."); 
+		return false; 
+	} 
+	else
+	{
+		confirm("Sure to Delete Data!");
+		return true; 
+	}
+	}); 
+});
+</script>
+</body>
+</html>
