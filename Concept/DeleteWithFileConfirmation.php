a43c1b02824201d544f860baa84e02d44895b2f1
Ashish <42094101+Candy128x@users.noreply.github.com>
2018-10-18 20:48:37 +0530

    Update DeleteWithFileConfirmation.php

--- Concept/DeleteWithFileConfirmation.php
+++ Concept/DeleteWithFileConfirmation.php
@@ -1,30 +1,32 @@
 <html>
 <head>
-<title>DeleteWithFileConfirmation</title>
+	<title>DeleteWithFileConfirmation</title>
 </head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
 <body>
 
 <form action="#" method="post">
-<input type="file" name="uploadFile" id="uploadFileId" size="30" /> 
-<input type="submit" name="submit" id="uploadSubmitId" value="Submit" />
+	<input type="file" name="uploadFile" id="uploadFileId" size="30" /> 
+	<input type="submit" name="submit" id="uploadSubmitId" value="Submit" />
 </form>
 
 <script type="text/javascript">
 $(document).ready(function() {
-	$('#uploadSubmitId').bind("click",function() 
-	{ 
+	$('#uploadSubmitId').bind("click",function() { 
 		var imgVal = $('#uploadFileId').val(); 
-	if(imgVal=='') 
-	{ 
-		alert("Please Select File.."); 
-		return false; 
-	} 
-	else
-	{
-		confirm("Sure to Delete Data!");
-		return true; 
-	}
+		if(imgVal=='') { 
+			alert("Please Select File.."); 
+			return false; 
+		} 
+		else {
+			var confirmation = confirm("Sure to Delete Data!");
+			if (confirmation ==true){
+				return true; 
+			}
+			else{
+				return false; 
+			} 
+		}
 	}); 
 });
 </script>
